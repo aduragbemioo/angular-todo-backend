7681f533398529783424f81ea9daa966f2e75838
aduragbemioo <aduragbemioo@users.noreply.github.com>
2017-02-06 15:53:32 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -12,32 +12,35 @@ app.use(cors());
 var db = mongoose.connect(process.env.MONGODB_URL ||'mongodb://localhost/todo');
 
 app.post('/task', function(req, res){
-  console.log('entered backend post');
+  console.log('Got to post route');
     var newTask = new Task(req.body);
     newTask.save(function (err, result) {
       if (err) return console.error(err);
         return res.send(result);
     });
-    console.log('saved in the backend');
 });
 
 app.get('/', function(req, res){
+  console.log('Got to index route');
   return res.send('Welcome!');
 });
 
-app.get('/task/:id', function(req, res){     
+app.get('/task/:id', function(req, res){
+  console.log('Got to findOne route'); 
   Task.findById(req.params.id, function(err, foundTask){
       return res.send(foundTask);
   });
 });
 
-app.get('/task', function(req, res){       
+app.get('/task', function(req, res){
+  console.log('Got to findAll route');       
   Task.find({},function(err, data){
       return res.json(data);
   });
 });
 
-app.put('/task/:id', function(req, res){     
+app.put('/task/:id', function(req, res){
+  console.log('Got to update route');
   console.log(req.body, req.params.id);
   Task.findById(req.params.id, function(err, foundTask){
     console.log(foundTask);
@@ -50,7 +53,7 @@ app.put('/task/:id', function(req, res){
 });
 
 app.delete('/task/:id', function(req, res){
-  console.log(req.params);
+  console.log('Got delete route to ' + req.params);
   Task.findByIdAndRemove(req.params.id, function(err, deletedTask){
       return res.send(deletedTask);
   });
